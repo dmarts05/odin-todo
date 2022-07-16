aa361a4d196dc495faab6a4f3dbe2f646f000bd6
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-07-16 15:10:29 +0200

    Add transition to sidebar toggle

--- src/scripts/sidebar-toggle.js
+++ src/scripts/sidebar-toggle.js
@@ -1,8 +1,17 @@
 function toggleSidebar() {
   const sidebar = document.querySelector('.sidebar');
   const projectView = document.querySelector('.project-view');
+
   sidebar.classList.toggle('sidebar-active');
   projectView.classList.toggle('sidebar-active');
+
+  changeSidebarToggleIcon();
+}
+
+function changeSidebarToggleIcon() {
+  const sidebarToggle = document.getElementById('sidebar-toggle');
+  sidebarToggle.classList.toggle('fa-angles-left');
+  sidebarToggle.classList.toggle('fa-angles-right');
 }
 
 function enableSidebarToggling() {
