db5ec77bd043df102f86643d49a51ac33f8c06d4
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-07-17 22:19:26 +0200

    Fix vh on mobile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles/index.scss';
+import fixVhOnMobile from './scripts/vh-fix-mobile';
 import enableSidebarToggling from './scripts/sidebar-toggle.js';
 
+fixVhOnMobile();
 enableSidebarToggling();
---
+++ src/scripts/vh-fix-mobile.js
@@ -0,0 +1,13 @@
+function setVhUnitVariable() {
+  // Get viewport height and multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+
+  // Set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+function fixVhOnMobile() {
+  window.addEventListener('resize', setVhUnitVariable);
+}
+
+export default setVhUnitVariable;
