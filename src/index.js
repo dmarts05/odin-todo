d4f2a68e601f6c07455568ef003a68b4361988e6
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-07-28 15:30:41 +0200

    Add unhide task text option for mobile users

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
 } from './scripts/ui/sidebar-toggle.js';
 import enableProjectSorting from './scripts/ui/sort-projects';
 import enableTaskSorting from './scripts/ui/sort-tasks';
+import enableCompactTextToggling from './scripts/ui/toggle-compact-text';
 import { AddModal } from './scripts/ui/add-modal';
 import { TaskModal } from './scripts/ui/task-modal';
 import { ProjectModal } from './scripts/ui/project-modal';
@@ -16,6 +17,7 @@ enableSidebarToggling();
 showSidebarAtWidth(700);
 enableProjectSorting();
 enableTaskSorting();
+enableCompactTextToggling(700, ['project-view__task__task-name']);
 
 // Create Modals
 const addModal = new AddModal('add-modal-toggle', 'add-modal');
