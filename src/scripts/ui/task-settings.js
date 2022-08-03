f4f044aeb334a853cce1b82095e9b18097d4e88d
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-08-03 20:13:25 +0200

    Add transition when removing a task

--- src/scripts/ui/task-settings.js
+++ src/scripts/ui/task-settings.js
@@ -4,17 +4,23 @@ import { updateProjectViewTasks } from './project-view-updater';
 import { getProjectWithTask } from '../logic/project-manager';
 
 function removeTask(e) {
-  const taskId = e.target.parentNode.parentNode.parentNode.dataset.taskId;
+  // Start slide transition
+  document.querySelector('.project-view__tasks').classList.add('slide');
 
-  const projectWithRemovedTask = getProjectWithTask(taskId);
+  setTimeout(() => {
+    const taskId = e.target.parentNode.parentNode.parentNode.dataset.taskId;
 
-  projectWithRemovedTask.removeTask(projectWithRemovedTask.getTask(taskId));
+    const projectWithRemovedTask = getProjectWithTask(taskId);
 
-  updateDefaultProjects();
-  updateSidebarProjects();
-  updateProjectViewTasks(
-    document.querySelector('.sidebar__project--active').dataset.projectId
-  );
+    projectWithRemovedTask.removeTask(projectWithRemovedTask.getTask(taskId));
+
+    updateDefaultProjects();
+    updateSidebarProjects();
+    updateProjectViewTasks(
+      document.querySelector('.sidebar__project--active').dataset.projectId
+    );
+    // End slide animation
+  }, 150);
 }
 
 function enableTaskRemoval() {
